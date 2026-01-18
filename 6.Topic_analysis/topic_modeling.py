3b00279b54ec112eb411a5e241bdef30ccf6cde0
Elena de la Fuente <124260430+eledelaf@users.noreply.github.com>
2026-01-18 09:01:28 +0000

    Update MongoDB URI to be commented and empty
    Comment out the MongoDB URI and set it to an empty string.

--- 6.Topic_analysis/topic_modeling.py
+++ 6.Topic_analysis/topic_modeling.py
@@ -31,7 +31,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 # -----------------------------
 # MongoDB connection
 # -----------------------------
-MONGO_URI = "mongodb+srv://eledelaf:Ly5BX57aSXIzJVde@articlesprotestdb.bk5rtxs.mongodb.net/?retryWrites=true&w=majority&appName=ArticlesProtestDB"
+# MONGO_URI = "mongodb+srv://eledelaf:Ly5BX57aSXIzJVde@articlesprotestdb.bk5rtxs.mongodb.net/?retryWrites=true&w=majority&appName=ArticlesProtestDB"
+MONGO_URI = " "
 DB_NAME = "ProjectMaster"
 COLLECTION_NAME = "Texts"
 
